e94a2f67eef048b26881fa81b789fd72b53c1aeb
futhinh <phuthinh.zone@gmail.com>
2026-02-04 08:45:10 +0700

    Them class LibraryManager

---
+++ src/LibraryManager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryManager {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+}
